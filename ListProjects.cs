44c1a4d805071611364ab9771c69eb2ae7ff7ec3
jamie <jbuckley760@gmail.com>
2020-07-11 16:44:56 +0100

    Initial commit

---
+++ ListProjects.cs
@@ -0,0 +1,24 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace AbstractMechanics.TimeTracking.Function
+{
+    public static class ListProjects
+    {
+        [FunctionName("ListProjects")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            string responseMessage = "OK";
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
